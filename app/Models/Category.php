1d9c81bb9b07a01f80e1c3563577a323d494b4a4
Chef <zsolt.horvath@wst.hu>
2022-06-25 14:24:27 +0200

    equipment publish condition

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -34,7 +34,7 @@ class Category extends Model implements Sortable
 
     public function equipments()
     {
-        return $this->hasMany(Equipment::class,'category_id');
+        return $this->hasMany(Equipment::class,'category_id')->where('published');
     }
 
     public function featured()
